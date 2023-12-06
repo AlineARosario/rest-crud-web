e2d8d5b3bc78ad59709ff13956e19e157a9a70f9
Aline Rosario <17.araujo@gmail.com>
2023-12-06 15:38:52 -0300

    add axios

---
+++ services/api.ts
@@ -0,0 +1,8 @@
+
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: 'http://192.168.68.154:3000',
+    timeout: 1000,
+
+  });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { api } from "../../services/api";
 
 const initialItens = [
   { id: 1, nome: "Banana" },
@@ -7,17 +8,46 @@ const initialItens = [
 ];
 
 export default function Home() {
+  const [textInput, setTextInput] = useState("");
   const [itens, setItens] = useState(initialItens);
 
   async function handleClick() {
-    const response = await fetch("http://192.168.68.154:3000/produtos");
-    const produtos = await response.json();
-    setItens(produtos);
-    console.log(produtos);
+    const response = await api.get("/produtos"); 
+    console.log(response); 
+    setItens(response.data);
+
+    //const response = await fetch("http://192.168.68.154:3000/produtos");
+    //const produtos = await response.json();
+    //console.log(produtos);
+  }
+
+  async function handleAddItem(){
+    console.log(textInput);
+    const data = { nome: textInput };  
+
+    try {
+      const response = await fetch("http://192.168.68.154:3000/produtos", {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Ocorreu um erro");
+    }
   }
 
   return (
     <main>
+    <div>
+      <input onChange={(e) => setTextInput(e.target.value)} placeholder="Digite o seu texto aqui..."></input>
+      <button onClick={handleAddItem}>Enviar</button>
+    </div>
       <button onClick={handleClick}>Buscar informação no servidor</button>
 
       <ul>
